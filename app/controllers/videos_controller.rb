d989c7e562be82d3bc258c17939961eb30c227aa
Eli <eli@Elis-Mac.local>
2011-09-13 18:18:56 +0300

    Updating show_video action (for bringing the video + validation +  URL redirection to URI)

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,10 @@
 class VideosController < ApplicationController
 	
 	def show_video
-		
+		video_id = params[:id].to_i
+		@video = Video.find(video_id) if video_id != 0
+		if !@video then render_404 and return end
+	  check_video_redirection(@video)
 	end
 	
 	def list
@@ -17,6 +20,12 @@ class VideosController < ApplicationController
 	  @order = "latest"
 	  @videos = []
   end
-  
+
+
+  def check_video_redirection(video)
+    if request.path != video.uri
+      redirect_to(request.request_uri.sub(request.path, video.uri), :status => 301)
+    end  
+  end  
   
 end
