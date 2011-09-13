f58ce5bfd1142fe0dcd85ff420a779590297b51a
Eli <eli@Elis-Mac.local>
2011-09-13 18:17:46 +0300

    Updating functions for user and videos paths on file system

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   
   has_many :videos
   
+  IMAGES_PATH = '/images/user_images/'
+  
+  def self.directory(user_id)
+    string_id = (user_id.to_s).rjust(9,"0")
+    "#{IMAGES_PATH}#{string_id[0..2]}/#{string_id[3..5]}/#{string_id[6..8]}"
+  end
+  
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -3,12 +3,20 @@ class Video < ActiveRecord::Base
   belongs_to :user, :dependent => :destroy
   #has_permalink :title, :as => :uri, :update => true
   # Check Why doesn't work??
+  
+  VIDEO_PATH = "/videos/"
+  
   def add_new_video(user_id,title)
     Video.create(:user_id => user_id, :title => title)
   end
 
   def uri
    "/video/#{id}-#{PermalinkFu.escape(title)}"
+  end
+
+  def self.directory(video_id)
+   string_id = (video_id.to_s).rjust(9,"0")
+   "#{VIDEO_PATH}#{string_id[0..2]}/#{string_id[3..5]}/#{string_id[6..8]}" 
   end
 
 end
