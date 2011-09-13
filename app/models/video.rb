188f1711782610b85307143402610ce275e646f5
Eli <eli@Elis-Mac.local>
2011-09-13 08:56:01 +0300

    Commenting permalinkfu attribute (doesnt work), adding uri function

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,14 +1,14 @@
 class Video < ActiveRecord::Base
 
   belongs_to :user, :dependent => :destroy
-  has_permalink :title, :update => true
-
+  #has_permalink :title, :as => :uri, :update => true
+  # Check Why doesn't work??
   def add_new_video(user_id,title)
     Video.create(:user_id => user_id, :title => title)
   end
 
-  def self.uri(id,title)
-    "/video/#{id}-#{PermalinkFu.escape(title)}"
+  def uri
+   "/video/#{id}-#{PermalinkFu.escape(title)}"
   end
 
 end
