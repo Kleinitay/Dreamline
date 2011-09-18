1509de516433ed62a5c053459b7c0db0ea40d6e6
Eli <eli@Elis-Mac.local>
2011-09-18 12:54:42 +0300

    Adding render_404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
 #  	@videos = Videos.get_by_latest
   @videos = []
   end
+  
+  def render_404
+      render(:file => "#{Rails.root}/public/404.html", :status => 404)
+  end
 end
