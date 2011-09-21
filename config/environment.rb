4a237a237d5966f664eb26cf7ff58f1f6735ee14
Elinor <elinor.dreamer@gmail.com>
2011-09-21 11:33:52 +0300

    Adding common data file + categories for video hash

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Dreamline::Application.initialize!
+
+CommonData = YAML::load(File.read('config/common_data.yml'))
