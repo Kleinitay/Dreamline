01a1243ef7be95536c4344e19f3e44a0477765bc
Elinor <elinor.dreamer@gmail.com>
2011-09-25 09:09:58 +0300

    moving commondata up

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+CommonData = YAML::load(File.read('config/common_data.yml'))
+
 # Initialize the rails application
 Dreamline::Application.initialize!
-
-CommonData = YAML::load(File.read('config/common_data.yml'))
