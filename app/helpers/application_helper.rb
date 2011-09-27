22d16f76c562e9bf53791823a11c1cbe49ba8b27
Elinor <elinor.dreamer@gmail.com>
2011-09-27 14:56:17 +0300

    Sidebars - changing file names + adding real data to video_list_box sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def nice_duration(seconds)
+    h = (seconds/3600).floor
+    m = ((seconds - (h * 3600))/60).floor
+    s = (seconds - (h * 3600 + m * 60)).round
+
+    # return formatted time without hours if = 0
+    hours = h == 0 ? "" : "#{'%02d' % h}:"
+    return "#{hours}#{'%02d:%02d' % [ m, s ]}"
+  end
 end
