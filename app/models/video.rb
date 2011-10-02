0f1494f8bd02b0801b00aa07a3d50170e8fb4be4
Elinor <elinor.dreamer@gmail.com>
2011-10-02 14:25:03 +0200

    Fixing ffmpeg source path

--- app/models/video.rb
+++ app/models/video.rb
@@ -132,7 +132,9 @@ class Video < ActiveRecord::Base
   def convert_command
     output_file = self.get_flv_file_name
     File.open(output_file, 'w')
-    command = <<-end_command ffmpeg -i #{ source.path } -ar 22050 -ab 32 -acodec libmp3lame -s 480x360 -vcodec flv -r 25 -qscale 8 -f flv -y #{ output_file } end_command
+    command = <<-end_command
+      ffmpeg -i #{ source.path } -ar 22050 -ab 32 -acodec libmp3lame -s 480x360 -vcodec flv -r 25 -qscale 8 -f flv -y #{ output_file }
+    end_command
     command.gsub!(/\s+/, " ")
   end
 # _____________________________________________ FLV conversion functions _______________________
