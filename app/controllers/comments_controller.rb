e616c3c803c2424107c2c95884b9f92bcfe0a41b
Elinor <elinor.dreamer@gmail.com>
2011-10-09 17:52:41 +0200

    Publishing comment and rendering a new view, added validations in model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,17 @@ class CommentsController < ApplicationController
 
   def create
     unless !signed_in? || !params[:comment]
-      more_params = {:user_id => current_user.id } #temp duration
+      more_params = {:user_id => current_user.id }
+      @ref = Video.uri(params[:comment][:video_id].to_i)
       @comment = Comment.new(params[:comment].merge(more_params))
       if @comment.save
-        flash[:notice] = 'Comment has been published'
-        redirect_to "/"
+        @done = true
       else
-        puts "no"
+        @done = false
       end
+      render "comment_published"
     else
-      redirect_to "/"
+      redirect_to ref
     end
   end
 
