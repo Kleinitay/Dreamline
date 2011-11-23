35bbc70b442ccfabd7280a4bd02f7444667b0769
Itay Klein <klein.itay@gmail.com>
2011-11-23 18:03:12 +0200

    facebook authentication

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,8 @@ Dreamline::Application.routes.draw do |map|
     match 'sign_up'  => 'users#new', :as => 'sign_up'
     match 'sign_in'  => 'sessions#new', :as => 'sign_in'
     match 'sign_out' => 'sessions#destroy', :as => 'destroy'
+    match 'auth' => 'sessions#aoth_athenticate', :as => 'aoth'
+    match 'auth_return' => 'sessions#aoth_athenticate_return', :as => 'aoth_return'
     
     
     # Why doesn't this work??
