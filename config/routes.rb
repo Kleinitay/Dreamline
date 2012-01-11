d0126830e50cf5b7ef8d05f66f47f8aa63701028
Elinor <elinor.dreamer@gmail.com>
2012-01-11 13:46:34 +0200

    removing commented lines

--- config/routes.rb
+++ config/routes.rb
@@ -54,35 +54,7 @@ Dreamline::Application.routes.draw do |map|
 
 #------------- Text -------------------------------------------------------------
   match 'about' => 'application#about', :as =>'about'
-  
-# ___________________Clearance routes______________________________________________________
-  #Delete later commented lines:
-    #resources :passwords,
-    #  :controller => 'clearance/passwords',
-    #  :only       => [:new, :create]
 
-    #resource  :session,
-    #  :controller => 'clearance/sessions',
-    #  :only       => [:new, :create, :destroy]
-
-    #resources :users, :controller => 'clearance/users', :only => [:new, :create] do
-    #  resource :password,
-    #    :controller => 'clearance/passwords',
-    #    :only       => [:create, :edit, :update]
-    #end
-    
-    # match 'sign_up'  => 'clearance/users#new', :as => 'sign_up'
-    #match 'sign_in'  => 'clearance/sessions#new', :as => 'sign_in'
-    #match 'sign_out' => 'clearance/sessions#destroy', :as => 'destroy'
-
-    #resource :session, :controller => 'sessions'
-    
-
-    
-    
-    # Why doesn't this work??
-    #match 'sign_out' => 'clearance/sessions#destroy', :via => :delete, :as => 'sign_out'
- #____________________________________________________________________________________________
   
   
 #____________________________________________________________________________________________
