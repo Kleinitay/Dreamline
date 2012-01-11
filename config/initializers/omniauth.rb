7de12cacc2e76e0493263fc9ee45b9c9665fb34f
Elinor <elinor.dreamer@gmail.com>
2012-01-11 10:24:56 +0200

    fixing user_videos instead of videos in omni

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,  Facebook::APP_ID,  Facebook::SECRET, {:scope => 'publish_stream, email, offline_access, videos', :client_options => {:ssl => {:ca_file => "#{Rails.root}/config/ca"}}}
+  provider :facebook,  Facebook::APP_ID,  Facebook::SECRET, {:scope => 'publish_stream, email, offline_access, user_videos', :client_options => {:ssl => {:ca_file => "#{Rails.root}/config/ca"}}}
 end
