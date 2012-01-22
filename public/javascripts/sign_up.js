f7e4e031e386bc11dcacbe2781a5fb0634e17063
Pnina Lorman <pnina@ubuntu.(none)>
2012-01-22 14:27:21 +0200

    changes to login page adding facebook button, hiding the regular login and adding a jquery function to show hide the general login

---
+++ public/javascripts/sign_up.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+// hide the menual signup display
+$('#manual_signup').hide();
+	$('#manual_signup_link').click(function(){
+		$('#manual_signup').slideToggle('slow');
+	});
+});
