14ccb0563761968ba78769a212e53818ff207cb5
Pnina Lorman <pnina@ubuntu.(none)>
2012-01-25 13:15:36 +0200

    changing the login facebook button and hiding the manual sigin in area

--- public/javascripts/sign_up.js
+++ public/javascripts/sign_up.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 // hide the menual signup display
+$('#manual_signup_wrapper').hide();
 $('#manual_signup').hide();
 	$('#manual_signup_link').click(function(){
 		$('#manual_signup').slideToggle('slow');
