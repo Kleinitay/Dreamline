c384ebf164779999ee8b3cce1aac4c93e42303ea
Elinor <elinor.dreamer@gmail.com>
2012-02-07 12:07:17 +0200

    updating detect_and_convert + removing access_token from method params

--- app/controllers/fb_videos_controller.rb
+++ app/controllers/fb_videos_controller.rb
@@ -64,7 +64,7 @@ class FbVideosController < ApplicationController
        more_params = {:user_id => current_user.id, :duration => 0} #temp duration
        @video = Video.new(params[:video].merge(more_params))
        if @video.save
-         @video.detect_and_convert(fb_graph,fb_access_token)
+         @video.detect_and_convert(fb_graph)
          flash[:notice] = "Video has been uploaded"
          redirect_to "/fb/#{@video.fbid}/edit_tags/new"
        else
@@ -90,7 +90,7 @@ class FbVideosController < ApplicationController
              }
     @video = Video.new(params)
     if @video.save
-      @video.detect_and_convert(fb_graph,fb_access_token)
+      @video.detect_and_convert(fb_graph)
       flash[:notice] = "Video has been uploaded"
       redirect_to "/fb/#{@video.fbid}/edit_tags/new"
     else
