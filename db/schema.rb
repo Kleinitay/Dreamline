34609275839f3760694c511527c4f14388639e21
Elinor <elinor.dreamer@gmail.com>
2012-02-26 09:10:24 +0200

    nada

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20120222082934) do
   end
 
   create_table "comments", :force => true do |t|
-    t.text     "content"
+    t.text     "content",    :null => false
     t.integer  "video_id",   :null => false
     t.integer  "user_id",    :null => false
     t.integer  "status"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20120222082934) do
     t.integer  "fb_id"
     t.string   "video_id",     :null => false
     t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "videos", :force => true do |t|
